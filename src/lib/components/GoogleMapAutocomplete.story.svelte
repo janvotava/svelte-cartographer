409e7c3ff4fa810c1bcd5e6ba63d5827ef4f24ce
Jan Votava <votava@deployment.cz>
2022-07-28 19:41:49 +0700

    Story: basic autocomplete usage demo

---
+++ src/lib/components/GoogleMapAutocomplete.story.svelte
@@ -0,0 +1,38 @@
+<script lang="ts" context="module">
+  import type { PageMeta } from "@vitebook/client"
+
+  export const __pageMeta: PageMeta = {
+    title: "GoogleMapAutocomplete",
+    description: "Autocomplete input for places.",
+  }
+</script>
+
+<script lang="ts">
+  import { GoogleMapAutocomplete, GoogleMapMarker } from "$lib"
+  import { Variant } from "@vitebook/client"
+  import GoogleMap from "./GoogleMap.svelte"
+
+  let lat = 13.736717
+  let lng = 100.523186
+
+  function onPlaceChange(place: google.maps.places.PlaceResult) {
+    if (place.geometry) {
+      const { location } = place.geometry
+
+      if (location) {
+        lat = location.lat()
+        lng = location.lng()
+      }
+    }
+  }
+</script>
+
+<Variant name="Autocomplete with map context">
+  <GoogleMap apiKey={import.meta.env.VITE_GMAPS_API_KEY} {lat} {lng} zoom={12}>
+    <GoogleMapAutocomplete
+      on:change={(e) => onPlaceChange(e.detail)}
+      class="text-xl mb-4 px-6 py-4 w-full"
+    />
+    <GoogleMapMarker {lat} {lng} />
+  </GoogleMap>
+</Variant>
