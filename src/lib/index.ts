12cf19d6861db5b98f12c5c6d477eb44f8e23e76
Jan Votava <votava@deployment.cz>
2022-07-28 20:28:52 +0700

    Forgot to export GoogleMapCanvas

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import GoogleMap from "./components/GoogleMap.svelte"
+import GoogleMapCanvas from "./components/GoogleMapCanvas.svelte"
 import GoogleMapMarker from "./components/GoogleMapMarker.svelte"
 import GoogleMapAutocomplete from "./components/GoogleMapAutocomplete.svelte"
 
-export { GoogleMap, GoogleMapMarker, GoogleMapAutocomplete }
+export { GoogleMap, GoogleMapCanvas, GoogleMapMarker, GoogleMapAutocomplete }
